dec793cae752597d257caa90730c974cc9fbda04
schap <schap@gurkensalat>
2020-11-27 15:26:44 +0100

    - jquery with my degugger installed - easy calculator with input done

---
+++ annas_calc.js
@@ -0,0 +1,45 @@
+// This will check if jQuery has loaded. If not, it will add to <head>
+window.onload = function() {
+  if (!window.jQuery) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://code.jquery.com/jquery-latest.min.js';
+    head.appendChild(script);
+  }
+}
+//debugger time out
+function defer(method) {
+  if (window.jQuery) {
+    method();
+  } else {
+    setTimeout(function() { defer(method) }, 50);
+  }
+}
+
+//creates a listener for when you press a key
+window.onkeyup = keyup;
+
+// global input variable
+var inputTextValue = "";
+var inputNumber = new Number(inputTextValue);
+
+// This will wait until Jquery is loaded then fire your logic
+defer(function () {
+  $("div").html("Enter your concetete qubic metres and press ENTER");
+  console.log(document);
+});
+
+function keyup(e) {
+  //setting your input text to the global Javascript Variable for every key press
+	inputNumber = e.target.value;
+  $('#searchValue').text("we calculate with:" + inputNumber);
+  //listens for you to press the ENTER key, at which point your web address will change to the one you have input in the search box
+  if (e.keyCode == 13) {
+	  var calcuteCO2 = inputNumber * 200;
+	  $('#searchValue').text("You can save: " + calcuteCO2 + " CO2 Emmisions");
+  }
+}
+
+
+	
