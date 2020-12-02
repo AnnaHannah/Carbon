5da31a5018498e37b30dd20f7290f5460f7ab343
schap <schap@gurkensalat>
2020-12-02 14:23:14 +0100

    - eclipse Ändert text codierung heimlich, problem erkannt

--- calc_new.js
+++ calc_new.js
@@ -32,17 +32,13 @@ window.onkeyup = keyup;
 var inputNumber = "";
 var inputNumber = new Number(inputNumber);
 
-//var inputTextValueEuro = "";
-//var inputNumEuro = new Number(inputTextValueEuro);
-
-
 function calculateAndDisplay(inputNumber) {
   //CO2 formula
   var calculatedCO2 = Math.round(inputNumber * 257.5556);
   // the " at the end of searchValue, please let it there, that is the only way this works, even if this violenced js norms
   if (Number.isNaN(calculatedCO2) == false) {
     if (inputNumber > 0) {
-      $('#searchValue').text(calculatedCO2 + " kg of CO₂ions can be safed by using our concrete, thats about 36% less");
+      $('#searchValue').text(calculatedCO2 + "  kg of CO2 emissions can be safed by using our concrete, thats about 36 %");
     }
   }
 }
@@ -53,16 +49,17 @@ function calculateAndDisplayPrice(inputNumber) {
   var calculatedCost = Math.round(inputNumber * 7466.85);
   if (Number.isNaN(calculatedCost) == false) {
     if (inputNumber > 0) {
-      $('#searchValueEuro').text(calculatedCost + " €the estimated cost for our concrete")
+      $('#searchValueEuro').text(calculatedCost + " € the estimated cost for our concrete ");
     }
   }
 }
 function calculateAndDisplayPriceDiff(inputNumber) {
   //Euro Differenz formula
   var calculatedCostDiff = Math.round(inputNumber * 0.99666392119);
-  if (Number.isNaN(calculatedCostDiff) == false) {
+  var x = calculatedCostDiff
+  if (Number.isNaN(x) == false) {
     if (inputNumber > 0) {
-      $('#searchValueEuroDiff').text(calculatedCostDiff + " € the cost-difference to conventional products")
+      $('#searchValueEuroDiff').text(x + " € is the cost difference to conventional products its about 1 % ");
     }
   }
 }
@@ -73,5 +70,4 @@ function keyup(e) {
   calculateAndDisplay(inputNumber);
   calculateAndDisplayPrice(inputNumber);
   calculateAndDisplayPriceDiff(inputNumber);
-
 }
